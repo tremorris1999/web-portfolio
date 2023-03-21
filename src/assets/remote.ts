2ed11172a11188b1a33baad6cfc64374f836a57a
Trevor Morris <tremorris1999@gmail.com>
2023-03-20 20:32:39 -0700

    chore: adds other repos

--- src/assets/remote.ts
+++ src/assets/remote.ts
@@ -1,9 +1,14 @@
 import { RepoLookup } from '@/types'
 
 export const repos: { [key: string]: RepoLookup } = {
-  'shop-gen': { owner: 'tremorris1999', name: 'shop-gen' },
   'speed-commerce': { owner: 'tremorris1999', name: 'speed-commerce' },
+  'shop-gen': { owner: 'tremorris1999', name: 'shop-gen' },
+  'web-portfolio': { owner: 'tremorris1999', name: 'web-portfolio' },
+  ActionifySharp: { owner: 'tremorris1999', name: 'ActionifySharp' },
+  DataAdapterSharp: { owner: 'tremorris1999', name: 'DataAdapterSharp' },
   PogSharp: { owner: 'tremorris1999', name: 'PogSharp' },
+  MirrorModel: { owner: 'tremorris1999', name: 'MirrorModel' },
+  Mirror: { owner: 'tremorris1999', name: 'Mirror' },
 }
 
 export const images: { [key: string]: string[] } = {
